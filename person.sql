96d116390ebf473c1a78040d58d939f37d1cc476
Jack <jackdonnell74@gmail.com>
2022-07-12 21:41:12 +0700

    first commit

---
+++ person.sql
@@ -0,0 +1,58 @@
+CREATE TABLE person (
+    person_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    age INT,
+    height INT,
+    city VARCHAR(255),
+    favorite_color VARCHAR(255),
+);
+INSERT INTO person (name, age, height, city, favorite_color)
+VALUES ('Jack', 21, 185, 'Bangkok', 'red'), ('James', 22, 186, 'Hanoi', 'blue');
+SELECT * FROM person ORDER BY height DESC;
+SELECT * FROM person ORDER BY height ASC;
+SELECT * FROM person ORDER BY age DESC;
+SELECT * FROM person WHERE age > 20;
+
+
+
+/* #1 */
+CREATE TABLE person ( person_id SERIAL PRIMARY KEY, name VARCHAR(200), age INTEGER, height INTEGER, city VARCHAR(200), favorite_color VARCHAR(200));
+
+/* #2 */
+INSERT INTO person ( name, age, height, city, favorite_color ) VALUES ( 'First Last', 21, 182, 'City', 'Color' );
+
+/* #3 */
+SELECT * FROM person ORDER BY height DESC;
+
+/* #4 */
+SELECT * FROM person ORDER BY height ASC;
+
+/* #5 */
+SELECT * FROM person ORDER BY age DESC;
+
+/* #6 */
+SELECT * FROM person WHERE age > 20;
+
+/* #7 */
+SELECT * FROM person WHERE age = 18;
+
+/* #8 */
+SELECT * FROM person WHERE age < 20 OR age > 30;
+
+/* #9 */
+SELECT * FROM person WHERE age != 27;
+
+/* #10 */
+SELECT * FROM person WHERE favorite_color != 'red';
+
+/* #11 */
+SELECT * FROM person WHERE favorite_color != 'red' AND favorite_color != 'blue';
+
+/* #12 */
+SELECT * FROM person WHERE favorite_color = 'orange' OR favorite_color = 'green';
+
+/* #13 */
+SELECT * FROM person WHERE favorite_color IN ( 'orange', 'green', 'blue' );
+
+/* #14 */
+SELECT * FROM person WHERE favorite_color IN ( 'yellow', 'purple' )
